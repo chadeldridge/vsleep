74303c78c9010031d0db65c85b2b7b53a3626651
Chad Eldridge <chad.eldridge@gmail.com>
2026-05-24 21:40:45 -0400

    Added commnts.

--- src/bin/vsleep/main.rs
+++ src/bin/vsleep/main.rs
@@ -7,11 +7,13 @@ mod cli;
 const DEFAULT_INTERVAL: u64 = 1;
 
 fn main() {
+    // Setup basic variables.
     let cli = cli::Cli::new(cli::Args::new());
     let verbose = cli.args.verbose;
     let date_format = cli.args.date_format.clone();
     let mut spinners = Spinners::default();
 
+    // Import spinners from provide file if a spinner file was provided.
     if !cli.args.file.is_empty() {
         match spinners.import_spinners(cli.args.file) {
             Ok(_) => {}
@@ -22,11 +24,14 @@ fn main() {
         };
     }
 
+    // If the --list option was passed, list all spinner names and exit.
     if cli.args.list {
         show_spinner_names(&spinners);
         std::process::exit(0);
     }
 
+    // Get the duration passed by the user. Duration is an option to allow us to have aruguements
+    // that do not require a duration to be passed, like --list.
     let duration = match cli.args.duration {
         Some(n) => n,
         None => {
@@ -35,10 +40,13 @@ fn main() {
         }
     };
 
+    // Create the timer.
     let mut t = Timer::new(
         Duration::from_secs(duration as u64),
         Duration::from_secs(DEFAULT_INTERVAL),
     );
+
+    // Get the spinner to be used.
     let mut s = match spinners.get_spinner(&cli.args.spinner) {
         Some(s) => s,
         None => {
@@ -47,6 +55,8 @@ fn main() {
         }
     };
 
+    // Run the timer and wait for return. Run will do all the work by executing
+    // the closure each tick.
     t.run(|tick| {
         let frame = s.frame();
         println!("{}", format_tick(&frame, tick, verbose, &date_format));
@@ -54,6 +64,7 @@ fn main() {
     });
 }
 
+// Format output for each tick.
 fn format_tick(frame: &str, tick: &TickData, verbose: u8, date_format: &str) -> String {
     match verbose {
         0 => frame.to_string(),
@@ -67,6 +78,7 @@ fn format_tick(frame: &str, tick: &TickData, verbose: u8, date_format: &str) -> 
     }
 }
 
+// List all spinner names.
 fn show_spinner_names(s: &Spinners) {
     println!("Spinner Names:");
     for n in s.keys() {
